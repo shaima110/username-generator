36e477ded29c9fb3c40b9d98dd8cf35dafb91ef4
shaima110 <shaimasyeda250@gmail.com>
2025-09-28 18:14:10 +0530

    refactoring

--- src/UsernameGenerator.java
+++ src/UsernameGenerator.java
@@ -1,7 +1,7 @@
 public class UsernameGenerator {
     public static void main(String[] args) {
         long number = (long)(Math.random() * 1_000_000_0000L); // random up to 10 digits
-        String username = "TR" + String.format("%010d", number); // always pad with 0s
-        System.out.println("UserName: "+username);
+        String userName = "TR" + String.format("%010d", number); // always pad with 0s
+        System.out.println("UserName: "+userName);
     }
 }
