0170bc6a1723e4036129ffd16d1f6cf4d043fa4c
shaima110 <shaimasyeda250@gmail.com>
2025-09-28 17:44:59 +0530

    username generation

---
+++ src/UsernameGenerator.java
@@ -0,0 +1,9 @@
+
+
+public class UsernameGenerator {
+    public static void main(String[] args) {
+        long number = (long)(Math.random() * 1_000_000_0000L); // random up to 10 digits
+        String username = "TR" + String.format("%010d", number); // always pad with 0s
+        System.out.println("UserName: "+username);
+    }
+}
